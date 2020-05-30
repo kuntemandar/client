a80ac9c85060380b482b0dfe99364a7ef4a89186
Mandar Kunte <mandar.kunte@walmartlabs.com>
2020-05-29 23:06:43 -0700

    initial commit

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+import Accordion from "react-bootstrap/Accordion";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h1>{room}</h1>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img src={closeIcon} alt="close icon" />
+      </a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
